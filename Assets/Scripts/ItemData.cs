dc526ed8d78a09ff652690a09e6135612acdba61
emeursing1022 <emeursing1022@gmail.com>
2024-01-14 10:08:23 -0600

    Initial commit

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+
+    [TextArea]
+    public string description;
+    public int value;
+}
